0415a23bed569c91336a9d9455ff26c1ce24f823
Daniel-developer5 <daniel.mudrak2005@gmail.com>
2020-05-02 18:24:58 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let requestURL = 'https://daniel-developer5.github.io/json-verses/verses.json'
+let request = new XMLHttpRequest
+
+let verse = document.querySelector(".verse")
+let link = document.querySelector(".verse-link")
+
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send()
+
+request.addEventListener('load', () => {
+    let cont = request.response
+    fill(cont)
+})
+
+let randNum
+let prevNum
+
+function rand() {
+    randNum = Math.floor(Math.random() * (11 - 1) + 1)
+    if (randNum === prevNum) {
+        rand()
+    }
+    prevNum = randNum
+}
+
+rand()
+
+function fill(jsonObj) {
+    verse.textContent = jsonObj[`verse${randNum}`]
+    link.textContent = jsonObj[`link${randNum}`]
+}
